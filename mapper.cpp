21e2c2c01bd1da2fa229fd1f109bbf5bf6717549
saaramahmoudi <sin29mim@gmail.com>
2021-11-09 10:43:16 -0500

    parsing input

--- mapper.cpp
+++ mapper.cpp
@@ -1,18 +1,107 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <bits/stdc++.h>
 
 using namespace std;
 
+
+struct RAM{
+    int id;//same as inp in ram array inside circuit
+    int circuit_id;
+    string mode;
+    int width;
+    int depth;
+};
+
+//store benchmark specification
+struct Circuit{
+    int num_of_logic;
+    vector<RAM> ram;
+};
+
+
+//global variables
+Circuit* circuits;
+int num_of_circuits;
+
+//init the circuits based on input file
+void init(){
+    circuits = new Circuit[num_of_circuits];
+}
+
+
 void parse_input(){
     ifstream testFile;
     testFile.open("logical_rams.txt");
-    
     string input;
+
+    //get the number of circuits in benchmark
+    testFile >> input; 
+    testFile >> num_of_circuits;
+    
+    init();
+
+    //get the header line and ignore
+    testFile >> input;//Circuit
+    testFile >> input;//RamID
+    testFile >> input;//Mode
+    testFile >> input;//Depth
+    testFile >> input;//Width
+    
+    int c_index = 0;
     while(!testFile.eof()){
-        testFile >> input;
-        cout << input << endl;
+        int inp;
+        //circuit id
+        testFile >> inp;
+        //input file last line is empty
+        if(inp < c_index){ 
+            break;
+        }
+        if(inp != c_index){
+            c_index = inp;
+        }
+        RAM ram_temp;
+        ram_temp.circuit_id = inp;
+        //RAM id
+        testFile >> inp;
+        ram_temp.id = inp;
+        //RAM mode
+        string mode;
+        testFile >> mode;
+        ram_temp.mode = mode;
+        //RAM depth
+        testFile >> inp;
+        ram_temp.depth = inp;
+        //RAM WIDTH
+        testFile >> inp;
+        ram_temp.width = inp;
+
+        circuits[c_index].ram.push_back(ram_temp);
     }
+
+    testFile.close();
+    
+    //read the second file
+    testFile.open("logic_block_count.txt");
+    //get the header line and ignore
+    testFile >> input;
+    testFile >> input;
+    testFile >> input;
+    testFile >> input;
+    testFile >> input;
+    testFile >> input;
+    testFile >> input;
+    
+    while (!testFile.eof()){
+        int circuit_id;
+        int circuit_logic_count;
+        testFile >> circuit_id;
+        testFile >> circuit_logic_count;
+        circuits[circuit_id].num_of_logic = circuit_logic_count;
+    }
+    
+    testFile.close();
 }
 
 
