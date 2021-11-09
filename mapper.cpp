f7a00ebf9140953d75a4425aa1af2d56dd44ccfe
saaramahmoudi <sin29mim@gmail.com>
2021-11-09 17:51:07 -0500

    8BRAM added

--- mapper.cpp
+++ mapper.cpp
@@ -8,6 +8,14 @@
 #define LUTRAM_MODE_0_WIDTH 10
 #define LUTRAM_MODE_1_WIDTH 20
 
+#define BRAM_8K_BITS 8192
+#define BRAM_128_BITS 131072
+
+#define LUTRAM_AREA 4
+#define LUT_AREA 3.5
+#define BRAM_8K_AREA 9.6505
+#define BRAM_128K_AREA 73.5343
+
 using namespace std;
 
 struct RAM{
@@ -114,9 +122,10 @@ void find_best_mapping(RAM r){
     cout << "MAPPING RAM " << r.mode << " " << r.depth << " " << r.width << endl;
     //MAP TO LUTRAM 
     int required_LUTRAM = -1;
-    int required_logic = -1;
-    double area = -1;
+    int required_LUTRAM_logic = -1;
+    double LUTRAM_required_area = -1;
     if(r.mode != "TrueDualPort"){
+        cout << "========LUTRAM========" << endl;
         //LUTRAM MODE 0
         int lut_mode_0 = 0;
         int lut_mode_0_logic = 0;
@@ -194,29 +203,76 @@ void find_best_mapping(RAM r){
 
         if(lut_mode_0 < 0){
             required_LUTRAM = lut_mode_1;
-            required_logic = lut_mode_1_logic;
+            required_LUTRAM_logic = lut_mode_1_logic;
         }
         if(lut_mode_1 < 0){
             required_LUTRAM = lut_mode_0;
-            required_logic = lut_mode_0_logic;
+            required_LUTRAM_logic = lut_mode_0_logic;
         }
-        if(lut_mode_1 > 0 && lut_mode_0 > 0){
-            if((4*lut_mode_0+3.5*lut_mode_0_logic) > (4*lut_mode_1+3.5*lut_mode_1_logic)){
+        if(lut_mode_1 >= 0 && lut_mode_0 >= 0){
+            if((LUTRAM_AREA*lut_mode_0+ LUT_AREA*lut_mode_0_logic) > (LUTRAM_AREA*lut_mode_1+LUT_AREA*lut_mode_1_logic)){
                 required_LUTRAM = lut_mode_1;
-                required_logic = lut_mode_1_logic;
+                required_LUTRAM_logic = lut_mode_1_logic;
             }
             else{
                 required_LUTRAM = lut_mode_0;
-                required_logic = lut_mode_0_logic;
+                required_LUTRAM_logic = lut_mode_0_logic;
             }
         }
-        cout << "========LUTRAM========" << endl;
-        cout << "LUTRAM " << required_LUTRAM << " " << required_logic << endl; 
     }   
+    //MIN area required by LUTRAM
+    if(required_LUTRAM >= 0 && required_LUTRAM_logic >= 0){
+        LUTRAM_required_area = LUTRAM_AREA * required_LUTRAM + LUT_AREA * required_LUTRAM_logic;
+    }
+    cout << "LUTRAM " << required_LUTRAM << " " << required_LUTRAM_logic << " " <<  LUTRAM_required_area << endl; 
 
     //MAP TO 8K BRAM
+    int required_BRAM8K = INT_MAX;
+    int required_BRAM8K_logic = 0;
+    double BRAM8K_required_area = DBL_MAX;
+    cout << "========8K BRAM========" << endl;
+    for(int x = 1 ; x <= 32; x *= 2){
+        //x32 is not available on TRUEDUALPORT mode!
+        if(r.mode == "TrueDualPort" && x == 32){
+            continue;
+        }
+        int req_ram = ceil((double)r.width/x) * ceil((double)r.depth/(BRAM_8K_BITS/x));
+        int req_logic = 0;
+        if(r.depth > (BRAM_8K_BITS/x)){//extra logic needed
+            if((double)r.depth/(BRAM_8K_BITS/x) > 16){
+                //can not be implemented
+                req_ram = -1;
+                req_logic = -1;
+            }
+            else{
+                int num_of_mux = (ceil((double)r.depth/(BRAM_8K_BITS/x))/4) + 1;
+                if((int) ceil((double)r.depth/(BRAM_8K_BITS/x)) % 4 == 1)
+                    num_of_mux--;
+                
+                req_logic += r.width * num_of_mux;
+                //decoder logic
+                int extra_rams = ceil((double)r.depth/(BRAM_8K_BITS/x));
+                if(extra_rams == 2){
+                    req_logic += 1; //only one LUT is required 
+                }
+                else{
+                    extra_rams = pow(2,ceil(log(extra_rams)/log(2)));
+                    req_logic += extra_rams;
+                } 
+            }
+        }
 
+        cout << "8k BRAM mode x" << x << " " << req_ram << " " << req_logic << endl;  
 
+        if(req_logic >= 0 && req_ram >= 0){
+            if(req_ram * BRAM_8K_AREA + req_logic * LUT_AREA < BRAM8K_required_area){
+                BRAM8K_required_area = req_ram * BRAM_8K_AREA + req_logic * LUT_AREA;
+                required_BRAM8K = req_ram;
+                required_BRAM8K_logic = req_logic;
+            }
+        }
+    }
+    cout << "BRAM 8K " << required_BRAM8K << " " << required_BRAM8K_logic << " " << BRAM8K_required_area << endl; 
     //MAP TO 128 BRAM
 
 }
